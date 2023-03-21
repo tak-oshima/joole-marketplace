c3dfa3a9435733df74bad02091c9da07eb48cde7
Takeyuki Oshima <simtak89@gmail.com>
2023-03-21 18:50:51 -0500

    Added ProjectProduct and Product entity classes to model.

---
+++ backend/src/main/java/com/itlize/joolemarketplace/model/ProjectProduct.java
@@ -0,0 +1,73 @@
+package com.itlize.joolemarketplace.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class ProjectProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer projectProductId;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(nullable = false)
+    private Project project;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(nullable = false)
+    private Product product;
+
+    public ProjectProduct() {
+    }
+
+    public Integer getProjectProductId() {
+        return projectProductId;
+    }
+
+    public void setProjectProductId(Integer projectProductId) {
+        this.projectProductId = projectProductId;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectProduct that = (ProjectProduct) o;
+        return Objects.equals(projectProductId, that.projectProductId) &&
+                Objects.equals(project, that.project) &&
+                Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(projectProductId, project, product);
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectProduct{" +
+                "projectProductId=" + projectProductId +
+                ", project=" + project +
+                ", product=" + product +
+                '}';
+    }
+
+    // TODO: Implement toJson() method here
+}
