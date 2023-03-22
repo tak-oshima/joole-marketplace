1ab8f2066d9b7ec0505e1b36a2578853add272d3
Chelsea☆Xi chen <chelseachenxi1999@gmail.com>
2023-03-22 21:31:38 +0800

    Create User class and Project class

---
+++ backend/src/main/java/com/itlize/joolemarketplace/model/User.java
@@ -0,0 +1,117 @@
+package com.itlize.joolemarketplace.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_type")
+    private String userType;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @OneToMany(fetch=FetchType.LAZY,
+            mappedBy="user",
+            cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Project> projects;
+
+    public User() {
+    }
+
+    public User(String userName, String userType, String userPassword, List<Project> projects) {
+        this.userName = userName;
+        this.userType = userType;
+        this.userPassword = userPassword;
+        this.projects = projects;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userName, user.userName) &&
+                Objects.equals(userType, user.userType) &&
+                Objects.equals(userPassword, user.userPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, userType,userPassword);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", userType='" + userType + '\'' +
+                ", userPassword=" + userPassword + '\'' +
+                '}';
+    }
+
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Project tempProject) {
+
+        if (projects == null) {
+            projects = new ArrayList<>();
+        }
+
+        projects.add(tempProject);
+
+        tempProject.setUser(this);
+    }
+
+    // TODO: Implement toJson() method here
+}
