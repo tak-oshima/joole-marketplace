463d24bac7f3f361b1b5bd19a52db15813da07ee
ZhangShuoyu <zhang.11636@osu.edu>
2023-03-24 10:09:16 -0400

    file name changed

--- backend/src/main/java/com/itlize/joolemarketplace/repository/TechnicalDetailRepository.java
+++ backend/src/main/java/com/itlize/joolemarketplace/repository/TechnicalDetailRepository.java
@@ -1,4 +1,4 @@
-package com.itlize.joolemarketplace.Repository;
+package com.itlize.joolemarketplace.repository;
 
 import com.itlize.joolemarketplace.model.TechnicalDetail;
 import com.itlize.joolemarketplace.model.Product;
